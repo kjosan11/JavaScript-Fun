22b0e761bbcc6cf5ed8a88dc1f0b73b8099485ba
kjosan11 <josank3311@gmail.com>
2021-02-22 16:51:17 -0800

    fixed css and the code for radio button. Now, one button will unselect if you select other

--- fancifymytext.js
+++ fancifymytext.js
@@ -2,20 +2,23 @@ function biggerText() {
   document.getElementById("bigText").style.fontSize = "24pt";
 }
 function styleText() {
+  document.getElementById("uncheckBoring").checked = false;
   document.getElementById("bigText").style.fontWeight = "bold";
-  document.getElementById("bigText").style.color = "blue";
+  document.getElementById("bigText").style.color = "#DC143C";
   document.getElementById("bigText").style.textDecoration = "underline";
 }
 function removeStyle() {
+  document.getElementById("uncheckFancy").checked = false;
   document.getElementById("bigText").style.fontSize = "inherit";
   document.getElementById("bigText").style.fontWeight = "inherit";
   document.getElementById("bigText").style.color = "inherit";
   document.getElementById("bigText").style.textDecoration = "none";
+  document.getElementById("bigText").style.textTransform = "none";
 }
 function upperCaseText() {
   document.getElementById("bigText").style.textTransform = "uppercase";
   var input = document.getElementById("bigText").value;
-  var parts = input.split("."); // ["How", "are", "you?"]
+  var parts = input.split(".");
   str = parts.join("-Moo");
   alert(str);
 }
